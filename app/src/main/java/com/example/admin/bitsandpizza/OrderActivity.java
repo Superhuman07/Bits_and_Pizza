55433020487add7351f1941a6fa18c117b59a513
Superhuman07 <bhatiarahul51@gmail.com>
2016-04-07 21:22:50 +0530

    Second Commit

---
+++ app/src/main/java/com/example/admin/bitsandpizza/OrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.admin.bitsandpizza;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_order);
+    }
+}
