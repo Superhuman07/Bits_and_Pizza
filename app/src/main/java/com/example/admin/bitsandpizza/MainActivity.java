ac842f2ffde8081140802ddaf46e29ac82d6ea98
Superhuman07 <bhatiarahul51@gmail.com>
2016-04-08 01:00:07 +0530

    Add Navigation Drawer

--- app/src/main/java/com/example/admin/bitsandpizza/MainActivity.java
+++ app/src/main/java/com/example/admin/bitsandpizza/MainActivity.java
@@ -1,19 +1,92 @@
 package com.example.admin.bitsandpizza;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
 
     private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
 
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+//Code to run when the item gets clicked
+            selectItem(position);
+        }
+    };
+    private void selectItem(int position)
+    {
+        Fragment fragment;
+        switch (position)
+        {
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+
+        }
+        // setActionBar title
+
+        setActionBarTitle(position);
+
+
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+
+        //now close the drawer
+
+        drawerLayout.closeDrawer(drawerList);
+    }
+    private void setActionBarTitle(int position)
+    {
+        String title;
+        if (position == 0)
+        {
+            title = getResources().getString(R.string.app_name);
+        }
+        else
+        {   // it has nbeen taken after creating the first activity
+            title = titles[position];
+        }
+        getActionBar().setTitle(title);
+    }
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (savedInstanceState == null)
+        {
+            selectItem(0);
+        }
+        // taking the titles here
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
         setContentView(R.layout.activity_main);
     }
 
